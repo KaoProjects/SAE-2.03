2bc4862ff22d2f20e305fdd4a288f49cee1f38fd
Sooolal <bonnel.emerand.solal@gmail.com>
2024-04-08 15:29:58 +0200

    Modifications et ajout de fichiers
    Modificiation du histoire-index/html
    Ajout du boostrap.php

--- passe_compose/pages/public/histoire-index.php
+++ passe_compose/pages/public/histoire-index.php
@@ -1,5 +1,9 @@
-<?php include("./../../Helpers/functions.php");
-echo head("Passé Composé")?>
+<?php 
+require './../../bootstrap.php';
+$sql = "SELECT * FROM histoire,participant where histoire.id_participant=participant.id_participant order by année_naissance";
+$histoires = ($dbh->query($sql)->fetchAll());
+echo head("Passé Composé")
+?>
 <body>
     <div id="logo">
         <img src="./../../assets/images/Logo PC.png" alt="">
@@ -12,6 +16,8 @@ echo head("Passé Composé")?>
         </div>
         <div id="podcastsList">
             <div class="podcast">
+                <?php foreach ($histoires as $histoire) {?>
+                
 
                 <p>podcast</p>
             </div>
