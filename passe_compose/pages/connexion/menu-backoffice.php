5afd8a143be364578c71f6028de86d405199a2d6
Sooolal <162103029+Sooolal@users.noreply.github.com>
2024-05-14 11:57:27 +0200

    Add files via upload

---
+++ passe_compose/pages/connexion/menu-backoffice.php
@@ -0,0 +1,20 @@
+<?php
+
+require './../../bootstrap.php';
+echo head("Back-office de Passé composé");
+?>
+
+<body style="padding:30px">
+	<nav>
+		<div>
+			<a href="#">Gestion des participants</a>
+		</div>
+		<div>
+			<a href="#">Gestion des histoires</a>
+		</div>
+		<div>
+			<a href="./deconnexion.php">Déconnexion</a>
+		</div>
+	</nav>
+</body>
+</html>
