80492a595451c0ba922dedae40f2ee265c994308
Viviane Laoue <viviane.laoue@gmail.com>
2024-05-16 11:26:26 +0200

    Modification id css

--- passe_compose/pages/public/histoire-index.php
+++ passe_compose/pages/public/histoire-index.php
@@ -11,8 +11,8 @@ echo head("Passé composé");
     <div id="banniere">
     </div>
     <img src="./../../assets/images/banniere.jpg" alt="Bannière" id="fond-img">
-    <div class="indexlogo">
-        <img src="./../../assets/images/Logo PC.png" alt="Logo Passé Composé" id="logoindex-img">
+    <div class="logoIndex">
+        <img src="./../../assets/images/Logo PC.png" alt="Logo Passé Composé" id="logoIndex-img">
     </div>
 
     <div id="paragraphe">
