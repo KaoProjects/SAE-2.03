f37af13a93af86c1a9a525e308d59b4600bd0639
Sooolal <bonnel.emerand.solal@gmail.com>
2024-05-16 11:16:08 +0200

    plein de trucs
    css

--- passe_compose/pages/public/histoire-detail.php
+++ passe_compose/pages/public/histoire-detail.php
@@ -14,13 +14,18 @@ echo head("Passé composé");
     <div id="logo">
         <img src="./../../assets/images/Logo PC.png" alt="">
     </div>
+    <div id="video-container">
+        <video autoplay muted loop>
+            <source src="./../../assets/images/FondDetail.mp4" type="video/mp4">
+            Votre navigateur ne supporte pas la lecture de vidéos au format HTML5.
+        </video>
+    </div>
 
     <div id="main-detail"> 
         <div id="podcastPage">
             <h1>Podcast de <?php echo $histoires['prénom']; ?></h1>
             <p><?php echo $histoires['résumé'];?></p>
             <?php echo $histoires["url_son"]?>
-
         </div>
     </div>
 </body>
